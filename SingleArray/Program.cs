46a5085241d7a2d07518dcdf9dbb3d3e5bdbef35
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-19 23:41:31 +0530

    Add project files.

---
+++ SingleArray/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num=new int[4] {4,6,7,8};
+            String[] s1 = { "vish", "mona", "rama", "seema" };
+                int[] number = new int[5];
+                string[] str = new string[5];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[i] = {num[i]}");
+            }
+
+            Console.WriteLine("--------------");
+
+            for(int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine($"s1[i] = {s1[i]}");
+            }
+
+            Console.WriteLine("-------------");
+           Console.WriteLine("enter 5 numbers");
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    number[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.Write("----------------");
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    Console.WriteLine($"number{i}={number[i]}");
+                }
+
+            Console.WriteLine("----------");
+            Console.WriteLine("enter 5 name");
+                  for (int i = 0; i < str.Length; i++)
+                     {
+                         str[i] = Console.ReadLine();
+                     }
+                     Console.WriteLine("----------------");
+
+                     for (int i = 0; i < str.Length;i++)
+                     {
+                         Console.WriteLine($"str{i} ={str[i]}");
+                     }
+        }
+    }
+}
